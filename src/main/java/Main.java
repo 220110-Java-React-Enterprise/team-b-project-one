e4316adc5209f6df4909cf8dd5d3804c0f13371d
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2022-02-09 14:37:29 -0700

    what?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,7 @@
-import ORM.ORM;
-import Objects.Flights;
-import Objects.Tickets;
-import Util.ConnectionManager;
-import Objects.Customers;
-import java.sql.Connection;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-//        try {
-//            Connection connection = ConnectionManager.getConnection();
-//
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        Scanner kb = new Scanner(System.in);
-//        int input = kb.nextInt();
-//        kb.nextLine();
-//    }
 
-
-
-
-
-
-
-        ORM orm = new ORM();
-        orm.connect("my-training-db.cmujmbzokloj.us-east-1.rds.amazonaws.com",
-                "3306", "ProjectOne","admin", "RevatureTraining713!");
-        Customers customer = new Customers();
-        Flights flight = new Flights();
-        //Tickets tickets = new Tickets();
-      // orm.ormEntry(flight, "create");
-      //orm.ormEntry(customer, "create");
-//        orm.ormEntry(tickets, "create");
-       orm.ormEntry(flight, "insert");
-        //orm.ormEntry(customer,"delete");
-        //orm.ormEntry(customer, "search");
-        //orm.ormEntry(customer,"update");
     }
 }
